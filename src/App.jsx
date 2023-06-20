f55830734ed86c7e20873649146fcd062fd51128
VERA MAGBUWE <veratinous@gmail.com>
2023-06-20 21:38:42 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "MAGBUWE Vera",
+  title: "Data Scientist",
   email: "alex@example.com",
   gitHub: "microsoft",
   instagram: "microsoft",
